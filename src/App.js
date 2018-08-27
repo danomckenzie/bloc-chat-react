b16001bef57eea9ffd98717bb1fab06cc80b3ea0
Dano McKenzie <danodanielmarie@gmail.com>
2018-08-27 09:42:34 -0600

    Bloc Chat: Room List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+<script src="https://www.gstatic.com/firebasejs/5.4.0/firebase.js"></script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAoOgZua1LZdw0PGDIlRm445RvhWGYbOts",
+    authDomain: "bloc-chat-c808b.firebaseapp.com",
+    databaseURL: "https://bloc-chat-c808b.firebaseio.com",
+    projectId: "bloc-chat-c808b",
+    storageBucket: "bloc-chat-c808b.appspot.com",
+    messagingSenderId: "264848437570"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
