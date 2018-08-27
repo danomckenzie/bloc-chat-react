5d2e89c444ca91303235b95049c9dc59e7846935
Dano McKenzie <danodanielmarie@gmail.com>
2018-08-27 10:03:55 -0600

    Bloc Chat: fixed list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList.js'
 import * as firebase from 'firebase';
 
 <script src="https://www.gstatic.com/firebasejs/5.4.0/firebase.js"></script>
@@ -19,13 +19,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <RoomList firebase = {firebase}/>
+        </div>
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,26 +1,31 @@
-import React, from { Component } from 'react';
+import React, { Component } from 'react';
 
 class RoomList extends Component {
   constructor(props) {
     super(props);
-      rooms: [],
+      this.state = {
+      rooms: []
   };
   this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.rooms.map((room, index) =>
+        <div key={index}>{room.name}</div>
+        )}
+      </div>
+    );
+  }
 }
 
-componentDidMount() {
- this.roomsRef.on('child_added', snapshot => {
-    const room = snapshot.val();
-    room.key = snapshot.key;
-    this.setState({ rooms: this.state.rooms.concat( room ) })
-  });
-}
-
-render() {
-  return (
-    <div>
-      {this.state.rooms.map((room, index) =>
-      <div key={index}>{room.name}</div>
-      )}
-  )
-}
+export default RoomList;
